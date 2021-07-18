b1cb0bdf5f9d65e8ec6c04a4a0cc7e623deba859
Peter Lohse <github@hsrvmail.net>
2021-07-18 11:36:06 +0200

    Umstellung auf PSR-12

--- files/lib/system/form/builder/field/wysiwyg/I18nWysiwygFormField.class.php
+++ files/lib/system/form/builder/field/wysiwyg/I18nWysiwygFormField.class.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace wcf\system\form\builder\field\wysiwyg;
+
 use wcf\system\form\builder\field\TI18nFormField;
 
-class I18nWysiwygFormField extends WysiwygFormField {
+class I18nWysiwygFormField extends WysiwygFormField
+{
     use TI18nFormField;
 
     protected $templateName = '__i18nWysiwygFormField';
--- make.sh
+++ make.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-rm -f acptemplates.tar
-7z a -ttar -mx=9 acptemplates.tar ./acptemplates/*
-rm -f files.tar
-7z a -ttar -mx=9 files.tar ./files/*
-rm -f templates.tar
-7z a -ttar -mx=9 templates.tar ./templates/*
-rm -f dev.hanashi.wsc.wysiwyg-i18n-form.tar
-7z a -ttar -mx=9 dev.hanashi.wsc.wysiwyg-i18n-form.tar ./* -x!acptemplates -x!files -x!templates -x!dev.hanashi.wsc.wysiwyg-i18n-form.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
+PACKAGE_NAME=dev.hanashi.wsc.wysiwyg-i18n-form
+PACKAGE_TYPES=(acptemplates files templates)
+
+for i in "${PACKAGE_TYPES[@]}"
+do
+    rm -rf ${i}.tar
+    7z a -ttar -mx=9 ${i}.tar ./${i}/*
+done
+
+rm -rf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}.tar.gz
+7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.phpcs.xml -x!.php_cs.dist -x!.github -x!ts -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json
+
+for i in "${PACKAGE_TYPES[@]}"
+do
+    rm -rf ${i}.tar
+done
